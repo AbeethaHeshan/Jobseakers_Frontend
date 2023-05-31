2e1977dd8b12cecbf21304ed3c48957407fc9cc6
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-06-01 00:47:47 +0530

    cliet signp carousel one created 1

--- app/signup/client/page.js
+++ app/signup/client/page.js
@@ -66,18 +66,20 @@ export default function SignUp() {
         </div>
         <div  style={{height:'92vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
             <div className='  box-shadow-type-one' style={{height:'500px',width:'700px',borderRadius:'15px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
-                     <span style={{fontSize:'30px'}} >Sign up as a client</span>
+                     <span style={{fontSize:'30px',fontFamily:'initial',fontWeight:'700',color:'#464755'}} >Sign up as a client</span>
                 <div style={{width:'80%',height:'260px'}}>        
                     <Carousel width={"620px"}   selectedItem={slideIndex} autoPlay={false} showStatus={false} showIndicators={false} >
-                        <div   style={{display:'flex',flexDirection:'column',justifyContent:'space-between',width:'90%',alignItems:'flex-start',height:"280px",padding:'20px' ,}}>
-                                <div style={{display:'flex', flexDirection : 'row',justifyContent:"space-between",columnGap:'20px'}}>
-                                    <TextField width={"200px"} height={"40px"} placeholder={"FirstName"} borderRadius={"10px"} borderColor={"red"}/>
-                                    <TextField width={"200px"} height={"40px"} placeholder={"LastName"}  borderRadius={"10px"} />
-                                </div>
-                                <TextField width={"300px"} height={"40px"} placeholder={"Email"}  borderRadius={"10px"} />
-                                <AuthField width={"300px"} height={"40px"} placeholder={"Username"} borderRadius={"10px"} type={"text"}/>
-                                <AuthField width={"300px"} height={"40px"} placeholder={"Password"} borderRadius={"10px"} type={"password"}/>
-                                <AuthField width={"300px"} height={"40px"} placeholder={"Confirm Password"} borderRadius={"10px"} type={"password"}/>
+                        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',width:'90%',alignItems:'center',height:"280px",padding:'20px'}}>
+                            <div style={{display:'flex',flexDirection:'column',justifyContent:'space-between',alignItems:'flex-start',height:'100%'}}>
+                                    <div style={{display:'flex', flexDirection : 'row',justifyContent:"space-between",columnGap:'20px'}}>
+                                        <TextField width={"200px"} height={"40px"} placeholder={"FirstName"} borderRadius={"10px"} borderColor={"red"}/>
+                                        <TextField width={"200px"} height={"40px"} placeholder={"LastName"}  borderRadius={"10px"} />
+                                    </div>
+                                    <TextField width={"300px"} height={"40px"} placeholder={"Email"}  borderRadius={"10px"} />
+                                    <AuthField width={"300px"} height={"40px"} placeholder={"Username"} borderRadius={"10px"} type={"text"}/>
+                                    <AuthField width={"300px"} height={"40px"} placeholder={"Password"} borderRadius={"10px"} type={"password"}/>
+                                    <AuthField width={"300px"} height={"40px"} placeholder={"Confirm Password"} borderRadius={"10px"} type={"password"}/>
+                            </div>   
                         </div>
                         <div>
                         
